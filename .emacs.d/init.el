dde367c79f1e35dedf598f16ed1d0311e001db54
Love Y <xlove.github@yregard.se>
2017-09-11 21:34:53 +0200

    emacs config file generation update
    init.el no longer overwrites itself. We put the result in gen-init.el
    instead. The reason for this is that we want a file that git doesn't
    track so we can add it to the gitignore file and don't get bothered when
    it is changing

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,12 +1,24 @@
-;; This file replaces itself with the actual configuration at first run.
+;; Note at the top of init.org file there is a line:
+;; #+PROPERTY: header-args :tangle gen-init.el
+;; When we tangle init.org the result will be outputted to gen-init.el
+
+;; This line will be added automatically anyways..
+(package-initialize)
 
 ;; We can't tangle without org!
 (require 'org)
-;; Open the configuration
-(find-file (concat user-emacs-directory "init.org"))
-;; tangle it
-(org-babel-tangle)
-;; load it
-(load-file (concat user-emacs-directory "init.el"))
-;; finally byte-compile it
-(byte-compile-file (concat user-emacs-directory "init.el"))
+
+;; The name and path of our generated file
+(setq gen-file (concat user-emacs-directory "gen-init.el"))
+
+;; If the generated file doesn't exist we should create it
+(unless (file-exists-p gen-file)
+  (progn
+    ;; Open the configuration
+    (find-file (concat user-emacs-directory "init.org"))
+    ;; tangle it
+    (org-babel-tangle))
+  )
+
+;; load the generated file
+(load-file gen-file)
