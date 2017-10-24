99f57c790c0bd18b22b118d33cb0639a33bc46ee
Love Yregård <xlove.github@yregard.se>
2017-10-24 22:33:57 +0200

    emacs config: refactored color theme

--- .emacs.d/custom_themes/mywombat2-theme.el
+++ .emacs.d/custom_themes/mywombat2-theme.el
@@ -2,21 +2,23 @@
   "Created 2017-10-01.")
 
 (let ((class '((class color) (min-colors 89)))
-      (loved-white   "#f6f3e8")
-      (loved-gray+2  "#99968b")
-      (loved-gray+1  "#444444")
-      (loved-gray    "#242424")
-      (loved-gray-1  "#1c1c1c")
-      (loved-pink+2  "#ff615a")
-      (loved-pink+1  "#f58c80")
-      (loved-pink    "#eea9b8")
-      (loved-green   "#92a65e")
-      (loved-green+1 "#95e454")
-      (loved-blue    "#8ac6f2")
-      (loved-red     "#e5786d")
-      (loved-yellow  "#cae682")
-      (loved-cyan    "#333366")
-      (loved-brown   "#ccaa8f")
+      (loved-white    "#f6f3e8")
+      (loved-gray+2   "#99968b")
+      (loved-gray+1   "#444444")
+      (loved-gray     "#242424")
+      (loved-gray-1   "#1c1c1c")
+      (loved-black    "#080808")
+      (loved-pink+2   "#ff615a")
+      (loved-pink+1   "#f58c80")
+      (loved-pink     "#eea9b8")
+      (loved-green    "#92a65e")
+      (loved-green+1  "#95e454")
+      (loved-blue     "#8ac6f2")
+      (loved-red      "#e5786d")
+      (loved-yellow   "#cae682")
+      (loved-yellow-1 "#ffe725")
+      (loved-cyan     "#333366")
+      (loved-brown    "#ccaa8f")
       )
 
   (custom-theme-set-variables
@@ -28,26 +30,26 @@
    'mywombat2
 
    ;; Basic colors
-   `(default             ((,class (:background ,loved-gray   :foreground ,loved-white)))) ; BG and FG
-   `(mode-line           ((,class (:background ,loved-white  :foreground ,loved-gray+1))))
-   `(mode-line-inactive  ((,class (:background ,loved-gray+1 :foreground ,loved-white))))
-   `(region              ((,class (:background ,loved-pink   :foreground ,loved-gray)))) ; Text selection
-   `(secondary-selection ((,class (:background ,loved-cyan   :foreground ,loved-white)))) ; Needed?
-   `(show-paren-match    ((,class (:background "#ff8700" :foreground "#080808"))))
-   `(show-paren-mismatch ((,class (:background ,loved-pink+2 :foreground ,loved-white))))
+   `(default             ((,class (:background ,loved-gray     :foreground ,loved-white)))) ; BG and FG
+   `(mode-line           ((,class (:background ,loved-white    :foreground ,loved-gray+1))))
+   `(mode-line-inactive  ((,class (:background ,loved-gray+1   :foreground ,loved-white))))
+   `(region              ((,class (:background ,loved-pink     :foreground ,loved-gray)))) ; Text selection
+   `(secondary-selection ((,class (:background ,loved-cyan     :foreground ,loved-white)))) ; Needed?
+   `(show-paren-match    ((,class (:background "#ff8700"       :foreground ,loved-black))))
+   `(show-paren-mismatch ((,class (:background ,loved-pink+2   :foreground ,loved-white))))
    `(minibuffer-prompt   ((,class (:foreground ,loved-red))))
    `(highlight           ((,class (:background ,loved-gray+1))))
-   `(link                ((,class (:foreground ,loved-blue :underline t))))
-   `(link-visited        ((,class (:foreground ,loved-red :underline t))))
+   `(link                ((,class (:foreground ,loved-blue     :underline t))))
+   `(link-visited        ((,class (:foreground ,loved-red      :underline t))))
    `(button              ((,class (:inherit (link)))))
-   `(isearch             ((,class (:background "#ff8700" :foreground "#080808"))))
-   `(lazy-highlight      ((,class (:background "#ffff00" :foreground "#080808"))))
-   `(header-line         ((,class (:background "#303030" :foreground "#e7f6da"))))
-   `(escape-glyph        ((,class (:foreground "#ddaa6f" :weight bold))))
+   `(isearch             ((,class (:background "#ff8700"       :foreground ,loved-black))))
+   `(lazy-highlight      ((,class (:background ,loved-yellow-1 :foreground ,loved-black))))
+   `(header-line         ((,class (:background "#303030"       :foreground "#e7f6da"))))
+   `(escape-glyph        ((,class (:foreground "#ddaa6f"       :weight bold))))
 
    ;; Basic colors under X
-   `(cursor ((,class (:background "#f6f3e8"))))
-   `(fringe ((,class (:background "#303030"))))
+   `(cursor ((,class (:background ,loved-white))))
+   `(fringe ((,class (:background ,loved-gray+1))))
 
    ;; Font locks
    `(font-lock-builtin-face       ((,class (:foreground ,loved-red))))
@@ -73,7 +75,13 @@
    `(company-tooltip-annotation           ((,class (:foreground ,loved-blue))))
    `(company-tooltip-common               ((,class (:foreground ,loved-yellow))))
    `(company-tooltip-annotation-selection ((,class (:foreground "#000080"))))
-   `(company-tooltip-common-selection     ((,class (:foreground "#8b0000")))))
+   `(company-tooltip-common-selection     ((,class (:foreground "#8b0000"))))
+
+   ;; Git-gutter
+   `(git-gutter:added    ((,class (:foreground ,loved-green+1))))
+   `(git-gutter:deleted  ((,class (:foreground ,loved-red))))
+   `(git-gutter:modified ((,class (:foreground ,loved-blue))))
+   )
   )
 
 (provide-theme 'mywombat2)
