7a33e658cc8ced5230e070aecf161385c361fa78
Love Yregård <love.yregard@consultant.volvo.com>
2020-06-04 16:17:02 +0200

    updated util scripts

--- utils/jumphost/install.sh
+++ utils/jumphost/install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 BASEDIR=$(readlink -f `dirname $0`)
-
-ln -s ${BASEDIR}/getdb ~/.ssh/getdb
+if [ -x "$(command -v dos2unix)" ]; then
+  dos2unix ${BASEDIR}/gettunnel
+fi
+ln -s ${BASEDIR}/gettunnel ~/.ssh/gettunnel
 
