a191ca1c7af268c196e18a50347f81205936373e
Love Yregård <xlove.github@yregard.se>
2024-08-30 17:00:00 +0200

    Emacs and wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -38,6 +38,8 @@ config.keys = {
 
 config.color_scheme = 'mywombat'
 config.font_size = 9.0
+-- Disable ligatures
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
 -- and finally, return the configuration to wezterm
 return config
